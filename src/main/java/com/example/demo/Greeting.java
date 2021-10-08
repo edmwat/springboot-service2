f0b31196eb67eb00172b0137aabbc69c5b838d17
edmwat <edmwat@gmail.com>
2021-10-08 20:46:49 +0300

    initial code commit

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Greeting {
+	public String greet;
+	public String name;
+	
+	public Greeting(String greet, String name) {
+		this.greet = greet;
+		this.name = name;
+	}
+}
