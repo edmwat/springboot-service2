71c60c506c076d4b78ab28394abdde9536f79f77
edmwat <edmwat@gmail.com>
2021-10-09 15:29:14 +0300

    changed from RestTemplate to Webclient

--- src/main/java/com/example/demo/GreetFactory.java
+++ src/main/java/com/example/demo/GreetFactory.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"https://jibtest-lhefrhbyia-uc.a.run.app","http://localhost:8080"}, maxAge = 3600)
 @RestController
 @RequestMapping(path="api/gfactory")
 public class GreetFactory {
